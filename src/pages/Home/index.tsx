0bb785a1a7092051490527104a73acb196700f59
Eduardo Parraga <eduardo.parraga95@gmail.com>
2021-09-10 14:32:03 -0300

    feat(home page mobile ready): finished mobile layout and basic form structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,65 +1,141 @@
-import { Heading, Button, Box } from "@chakra-ui/react";
+import { Heading, Button, Box, Input, UnorderedList, ListItem } from "@chakra-ui/react";
 
 import Flask from "../../assets/home_flask.svg";
 import Screen from "../../assets/home_screen.svg";
 import MobileBG from "../../assets/home_back_mobile.jpg";
+import Or from "../../assets/home_or.png";
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 const Home = () => {
-    return (
-        <div>
-            {/* <Box boxShadow="0px 0px 20px rgba(0, 0, 0, 0.1)" borderRadius="3xl" p="4">
-        <Heading variant="h1">teste</Heading>
-        <Button variant="disabled">Welcome</Button>
-      </Box> */}
 
-            {/* Aqui vai o Header */}
-            <Box
-                display='flex'
-                flexFlow='column nowrap'
-                alignItems='center'
-                justifyContent='center'
-                padding='16px'
-                textAlign='center'
-                bg='blue.300'>
+  const formSchema = yup.object().shape({
+    username: yup
+        .string()
+        .required('Usuário obrigatório'),
+    password: yup
+        .string()
+        .required('Senha obrigatória')
+  })
 
-                <Heading variant='h1' marginBottom='8'>
-                    Otimização de processos com inteligência e tecnologia
-                </Heading>
+  const { register, handleSubmit, formState: {errors}} = useForm({
+    resolver: yupResolver(formSchema)
+  })
 
-                <Box
-                    display='flex'
-                    flexFlow='row nowrap'
-                    alignItems='center'
-                    justifyContent='center'>
-                    <img width='120px' src={Flask} alt='frasco'></img>
-                    <img width='120px' src={Screen} alt='tela'></img>
-                </Box>
-            </Box>
+  const onFormSubmit = ( ) => {
+    
+  }
 
-            <Box
-                display='flex'
-                flexFlow='column nowrap'
-                alignItems='center'
-                justifyContent='center'
-                padding='16px'
-                textAlign='center'
-                bgImage={MobileBG}>
+  return (
+    <div>
 
-                <Box
-                    boxShadow='0px 0px 20px rgba(0, 0, 0, 0.1)'
-                    borderRadius='3xl'
-                    p='4'>
-                </Box>
-            </Box>
-            
-            <div className='middleContainer'>
-                <Box
-                    boxShadow='0px 0px 20px rgba(0, 0, 0, 0.1)'
-                    borderRadius='3xl'
-                    p='4'></Box>
-            </div>
-        </div>
-    );
+      {/* Aqui vai o Header */}
+
+      <Box
+        display='flex'
+        flexFlow='column nowrap'
+        alignItems='center'
+        justifyContent='center'
+        padding='16px'
+        textAlign='center'
+        bg='blue.300'>
+
+        <Heading variant='h1' marginBottom='8'>
+          Otimização de processos com inteligência e tecnologia
+        </Heading>
+
+        <Box
+          display='flex'
+          flexFlow='row nowrap'
+          alignItems='center'
+          justifyContent='center'>
+          <img width='120px' src={Flask} alt='frasco'></img>
+          <img width='120px' src={Screen} alt='tela'></img>
+        </Box>
+
+      </Box>
+
+      <Box
+        display='flex'
+        flexFlow='column nowrap'
+        alignItems='center'
+        justifyContent='center'
+        padding='48px 0px'
+        textAlign='center'
+        bgImage={MobileBG}>
+
+        <Box
+          bgColor='gray.50'
+          w='280px'
+          boxShadow='0px 0px 20px rgba(0, 0, 0, 0.1)'
+          borderRadius='3xl'
+          p='10px'>
+
+          <Heading variant='h4' mb='8'>
+            Digite suas credenciais
+          </Heading>
+
+          <Input
+            variant='flushed'
+            marginBottom='1px solid'
+            borderColor='blue.600'
+            placeholder='Usuário'
+            mb='4'/>
+
+          <Input
+            variant='flushed'
+            marginBottom='1px solid'
+            borderColor='blue.600'
+            placeholder='Senha'
+            mb='4'/>
+
+          <Box
+            display='flex'
+            flexFlow='column nowrap'
+            alignItems='center'
+            justifyContent='center'>
+
+            <Button w='260px' variant='default' mb='4' mt='4'>
+              Entrar
+            </Button>
+
+            <img width='32px' src={Or} alt='frasco'></img>
+
+            <Button w='260px' variant='default' mb='4' mt='4'>
+              Pesquise uma amostra
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        display='flex'
+        flexFlow='column nowrap'
+        alignItems='center'
+        justifyContent='center'
+        padding='16px'
+        textAlign='center'
+        bg='blue.300'>
+        
+        <Heading variant='h4' mb='4' color='gray.50' stroke='1px black'>
+          Por quê a TechSamples?
+        </Heading>
+
+        <UnorderedList
+          color='gray.50'
+          textAlign='left'>
+
+          <ListItem>Agilidade</ListItem>
+          <ListItem>Disponibilidade</ListItem>
+          <ListItem>Garantia de resultados</ListItem>
+          <ListItem>Confidenciabilidade</ListItem>
+          
+        </UnorderedList>
+      </Box>
+    </div>
+  );
 };
 
 export default Home;
