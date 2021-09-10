a424abc6818ecb16a6e5603fe78f0cc47d193411
Felipe Silva <felipesilva.98ads@gmail.com>
2021-09-10 10:13:35 -0300

    fix(auth provider): fix AuthProvider line 24 and line 26

--- src/providers/Auth/index.tsx
+++ src/providers/Auth/index.tsx
@@ -21,9 +21,9 @@ interface Props {
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }: Props) => {
-  const token = localStorage.getItem("token") || "";
+  const token = localStorage.getItem("token") || "[]";
   const [auth, setAuth] = useState<string>(token);
-  const localUser = localStorage.getItem("user") || "";
+  const localUser = localStorage.getItem("user") || "[]";
   const [user, setUser] = useState(JSON.parse(localUser));
   const history = useHistory();
 
