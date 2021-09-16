36726f03fde1cac47fc9c9701a3750497a12399b
Estevan T. D. Martins <estevandias7@gmail.com>
2021-09-16 15:49:31 -0300

    fix: showing error message to user;

--- src/components/AdminClassNewParams/index.tsx
+++ src/components/AdminClassNewParams/index.tsx
@@ -1,133 +1,138 @@
-import { Button, Input, Flex, Select } from "@chakra-ui/react"
-import { useClass } from "../../providers/Class"
+import { Button, Input, Flex, Select, Text } from "@chakra-ui/react";
+import { useClass } from "../../providers/Class";
 import { useParams } from "react-router-dom";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 export interface INewParamsForm {
-    an_name: string,
-    name: string,
-    unit: string,
-    vmin: string,
-    vmax: string,
+  an_name: string;
+  name: string;
+  unit: string;
+  vmin: string;
+  vmax: string;
 }
-
 
 const AdminClassNewParams = () => {
+  const { id } = useParams<{ id: string }>();
+  const {
+    classAnalyses,
+    fetchClass,
+    resetClass,
+    addClassTypeParams,
+    updateTrigger,
+  } = useClass();
 
-    const { id } = useParams<{ id: string }>();
-    const { classAnalyses, fetchClass, resetClass, addClassTypeParams, updateTrigger } = useClass();
+  const RefReset = useRef(resetClass);
+  const RefFetch = useRef(fetchClass);
 
-    const RefReset = useRef(resetClass);
-    const RefFetch = useRef(fetchClass);
+  useEffect(() => {
+    RefFetch.current(id);
 
-    useEffect(() => {
-        RefFetch.current(id);
-    
-        return RefReset.current;
-    }, [id, updateTrigger]);
+    return RefReset.current;
+  }, [id, updateTrigger]);
 
-    const formSchema = yup.object().shape({
-        an_name: yup
-            .string()
-            .required('Campo obrigatório'),
-        name: yup
-            .string()
-            .required('Campo obrigatório'),
-        unit: yup
-            .string()
-            .required('Campo obrigatório'),
-        vmin: yup
-            .string()
-            .required('Campo obrigatório'),
-        vmax: yup
-            .string()
-            .required('Campo obrigatório'),
-    });
-    
-    const { register, handleSubmit, formState: {errors}} = useForm({
-        resolver: yupResolver(formSchema)
-    })
+  const formSchema = yup.object().shape({
+    an_name: yup.string().required("Campo obrigatório"),
+    name: yup.string().required("Campo obrigatório"),
+    unit: yup.string().required("Campo obrigatório"),
+    vmin: yup.string().required("Campo obrigatório"),
+    vmax: yup.string().required("Campo obrigatório"),
+  });
 
-    const onFormSubmit = ( formData: INewParamsForm ) => {
-        addClassTypeParams(id, formData)
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
 
-    return (
-    <Flex
-        flexFlow='column nowrap'
-        justifyContent='center'
-        alignItems='center'>
+  const onFormSubmit = (formData: INewParamsForm) => {
+    addClassTypeParams(id, formData);
+  };
 
-        <form
-            onSubmit={handleSubmit(onFormSubmit)}>
-            <Flex
-                mb='8'
-                flexFlow='column nowrap'
-                justifyContent='center'
-                alignItems='center'>
+  return (
+    <Flex flexFlow="column nowrap" justifyContent="center" alignItems="center">
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <Flex
+          mb="8"
+          flexFlow="column nowrap"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Select
+            variant="flushed"
+            marginBottom="1px solid"
+            borderColor="blue.600"
+            placeholder="Tipo de análise"
+            {...register("an_name")}
+            error={errors.an_name?.message}
+          >
+            {!!classAnalyses.length &&
+              classAnalyses.map((item) => (
+                <option key={item.an_name} value={item.an_name}>
+                  {item.an_name}
+                </option>
+              ))}
+          </Select>
+          <Text color="red.600" fontSize={13} h={5}>
+            {errors.an_name?.message}
+          </Text>
 
-                <Select
-                    variant="flushed"
-                    marginBottom="1px solid"
-                    borderColor="blue.600"
-                    placeholder='Tipo de análise'
-                    {...register('an_name')}
-                    error={errors.an_name?.message}>
-                    
-                    {!!classAnalyses.length && classAnalyses.map(item => 
-                        <option key={item.an_name} value={item.an_name}>{item.an_name}</option>)}
-
-                </Select>
-                        
-                <Input
-                    variant="flushed"
-                    marginBottom="1px solid"
-                    borderColor="blue.600"
-                    placeholder='Nome'
-                    {...register('name')}
-                    error={errors.name?.message}/>
-
-                <Input
-                    variant="flushed"
-                    marginBottom="1px solid"
-                    borderColor="blue.600"
-                    placeholder='Unidade'
-                    {...register('unit')}
-                    error={errors.unit?.message}/>
-
-                <Input
-                    variant="flushed"
-                    marginBottom="1px solid"
-                    borderColor="blue.600"
-                    placeholder='Valor mínimo'
-                    {...register('vmin')}
-                    error={errors.vmin?.message}/>
-
-                <Input
-                    variant="flushed"
-                    marginBottom="1px solid"
-                    borderColor="blue.600"
-                    placeholder='Valor máximo'
-                    {...register('vmax')}
-                    error={errors.vmax?.message}/>
-
-                <Button
-                    type='submit'
-                    variant='default'
-                    mt='8'>
-                    Cadastrar
-                </Button>
-            </Flex>
-        </form>
-
-
-
+          <Input
+            variant="flushed"
+            marginBottom="1px solid"
+            borderColor="blue.600"
+            placeholder="Nome"
+            {...register("name")}
+            error={errors.name?.message}
+          />
+          <Text color="red.600" fontSize={13} h={5}>
+            {errors.name?.message}
+          </Text>
+          <Input
+            variant="flushed"
+            marginBottom="1px solid"
+            borderColor="blue.600"
+            placeholder="Unidade"
+            {...register("unit")}
+            error={errors.unit?.message}
+          />
+          <Text color="red.600" fontSize={13} h={5}>
+            {errors.unit?.message}
+          </Text>
+          <Input
+            variant="flushed"
+            marginBottom="1px solid"
+            borderColor="blue.600"
+            placeholder="Valor mínimo"
+            {...register("vmin")}
+            error={errors.vmin?.message}
+          />
+          <Text color="red.600" fontSize={13} h={5}>
+            {errors.vmin?.message}
+          </Text>
+          <Input
+            variant="flushed"
+            marginBottom="1px solid"
+            borderColor="blue.600"
+            placeholder="Valor máximo"
+            {...register("vmax")}
+            error={errors.vmax?.message}
+          />
+          <Text color="red.600" fontSize={13} h={5}>
+            {errors.vmax?.message}
+          </Text>
+          <Button type="submit" variant="default" mt="8">
+            Cadastrar
+          </Button>
+        </Flex>
+      </form>
     </Flex>
-    )
-}
+  );
+};
 
-export default AdminClassNewParams
+export default AdminClassNewParams;
